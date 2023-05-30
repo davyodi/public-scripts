ddd2edaac3e3a7472f2136437129280bb9a05381
Davy Debontridder <98428453+davyodi@users.noreply.github.com>
2023-05-30 23:52:09 +0200

    Update provision-deb-ubu.sh

--- provision-deb-ubu.sh
+++ provision-deb-ubu.sh
@@ -31,7 +31,7 @@ install_docker_ubuntu() {
     sudo apt-get install ca-certificates curl gnupg -y
 
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     echo \
